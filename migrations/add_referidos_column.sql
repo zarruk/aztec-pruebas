d5b2f40eb856d96d2e8a7236dddb1482b8635a01
Salomon <szarruk@gmail.com>
2025-03-05 19:48:46 -0500

    Corregir tipo de dato de la columna referido_por en la migración

--- migrations/add_referidos_column.sql
+++ migrations/add_referidos_column.sql
@@ -1,5 +1,5 @@
 -- Agregar columna referido_por a la tabla registros_talleres
-ALTER TABLE registros_talleres ADD COLUMN referido_por UUID REFERENCES usuarios(id);
+ALTER TABLE registros_talleres ADD COLUMN referido_por INTEGER REFERENCES usuarios(id);
 
 -- Crear índice para mejorar el rendimiento de las consultas
 CREATE INDEX idx_registros_referido_por ON registros_talleres(referido_por); 
