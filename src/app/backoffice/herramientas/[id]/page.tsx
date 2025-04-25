ff43476f5d57e80dbd8064f96557f3b81ff1b885
Salomon zarruk <salomonzarruk@gmail.com>
2025-04-25 17:12:40 -0500

    Simplify page component

--- src/app/backoffice/herramientas/[id]/page.tsx
+++ src/app/backoffice/herramientas/[id]/page.tsx
@@ -1,17 +1,5 @@
-import { Metadata } from 'next';
 import EditarHerramienta from '@/components/backoffice/editar-herramienta';
 
-export const metadata: Metadata = {
-  title: 'Editar Herramienta - Backoffice',
-  description: 'Editar herramienta en el backoffice'
-};
-
-interface PageProps {
-  params: {
-    id: string;
-  };
-}
-
-export default function EditarHerramientaPage({ params }: PageProps) {
+export default function Page({ params }: { params: { id: string } }) {
   return <EditarHerramienta id={params.id} />;
 } 
